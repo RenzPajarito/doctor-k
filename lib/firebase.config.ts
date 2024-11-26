5f17b402e7cdc76e25068f37ac8b1cd87868f6f3
Renz Pajarito <renzpajarito08@gmail.com>
2024-11-26 10:45:47 +0800

    fix

--- lib/firebase.config.ts
+++ lib/firebase.config.ts
@@ -4,13 +4,13 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCDrhw7y4bBMct9J7BC9mULZfe0di32LK0",
+  authDomain: "exam-quiz-master.firebaseapp.com",
+  projectId: "exam-quiz-master",
+  storageBucket: "exam-quiz-master.appspot.com",
+  messagingSenderId: "282350778503",
+  appId: "1:282350778503:web:81810d31561ff33d8fa022",
+  measurementId: "G-VP48J9XMXG"
 };
 
 const app = initializeApp(firebaseConfig);
