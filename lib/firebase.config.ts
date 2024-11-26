5b5cc777c9be01383a80d4a242de61a2491560d1
Renz Pajarito <renzpajarito08@gmail.com>
2024-11-26 10:48:39 +0800

    fix env

--- lib/firebase.config.ts
+++ lib/firebase.config.ts
@@ -4,13 +4,13 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDrhw7y4bBMct9J7BC9mULZfe0di32LK0",
-  authDomain: "exam-quiz-master.firebaseapp.com",
-  projectId: "exam-quiz-master",
-  storageBucket: "exam-quiz-master.appspot.com",
-  messagingSenderId: "282350778503",
-  appId: "1:282350778503:web:81810d31561ff33d8fa022",
-  measurementId: "G-VP48J9XMXG"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
